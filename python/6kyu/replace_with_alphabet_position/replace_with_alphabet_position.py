94ba170d2d3cc24a14509e0fbdaaceb474873dda
Tofu-Gang <tofugangsw@gmail.com>
2026-07-24 19:12:21 +0200

    Python 6kyu Replace With Alphabet Position

---
+++ python/6kyu/replace_with_alphabet_position/replace_with_alphabet_position.py
@@ -0,0 +1,28 @@
+__author__ = "Tofu-Gang"
+__email__ = "tofugangsw@gmail.com"
+
+"""
+https://www.codewars.com/kata/546f922b54af40e1e90001da
+
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+Input = "The sunset sets at twelve o' clock."
+Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+"""
+
+def alphabet_position(text):
+    """
+    :param text: input string
+    :return: list of alphabet index for every letter in the input string
+    """
+
+    return " ".join(str(ord(c.lower()) - ord("a") + 1)
+                    for c in text.replace(" ", "")
+                    if not c.isnumeric() and c.isalpha())
